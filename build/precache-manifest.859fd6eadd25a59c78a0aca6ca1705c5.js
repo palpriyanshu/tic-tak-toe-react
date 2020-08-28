4ba50a34377f04b9f43a606e17bbb8deb6fb02c5
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-08-28 14:56:20 +0530

    added build

---
+++ build/precache-manifest.859fd6eadd25a59c78a0aca6ca1705c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfec2df91e0d142e980bae0cb2973f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d550257129f1b380fad",
+    "url": "/static/css/main.2f63928a.chunk.css"
+  },
+  {
+    "revision": "8c95aec151a4d5baebe0",
+    "url": "/static/js/2.f468cfb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f468cfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d550257129f1b380fad",
+    "url": "/static/js/main.dbe6b0cc.chunk.js"
+  },
+  {
+    "revision": "221280f60ef542e3ff3c",
+    "url": "/static/js/runtime-main.a482d232.js"
+  }
+]);
